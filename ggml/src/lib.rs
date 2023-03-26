eef35bc07e88fd0e47c5893772de854662ccb431
Philpax <me@philpax.me>
2023-03-26 21:32:39 +0200

    docs(ggml): Document library

--- ggml/src/lib.rs
+++ ggml/src/lib.rs
@@ -1,3 +1,13 @@
+#![deny(missing_docs)]
+
+//! `ggml` is a semi-idiomatic wrapper for the `ggml` C library.
+//!
+//! It exposes a subset of operations (currently used to implement the [llama-rs](https://crates.io/crates/llama-rs) library).
+//! Note that it does not expose a fully-idiomatic safe Rust interface; operations that could be potentially unsafe are marked as such.
+//!
+//! `ggml` operates on a computational graph; no values will be computed until [Context::graph_compute] is executed.
+//! All [Tensor]s are nodes in this computational graph, and values cannot be retrieved until computation is completed.
+
 use std::{
     ffi::c_void,
     ptr::NonNull,
@@ -6,27 +16,36 @@ use std::{
 
 pub use ggml_sys::ggml_type as Type;
 
+/// Magic constant for `ggml` files (versioned).
 pub const FILE_MAGIC: i32 = 0x67676d66;
+/// Magic constant for `ggml` files (unversioned).
 pub const FILE_MAGIC_UNVERSIONED: i32 = 0x67676d6c;
 
+/// The currently-supported format version for `ggml` files.
 pub const FORMAT_VERSION: u32 = 1;
 
+/// Quantized 4-bit (type 0).
 pub const TYPE_Q4_0: ggml_sys::ggml_type = ggml_sys::GGML_TYPE_Q4_0;
+/// Quantized 4-bit (type 1); used by GPTQ.
 pub const TYPE_Q4_1: ggml_sys::ggml_type = ggml_sys::GGML_TYPE_Q4_1;
+/// Integer 32-bit.
 pub const TYPE_I32: ggml_sys::ggml_type = ggml_sys::GGML_TYPE_I32;
+/// Float 16-bit.
 pub const TYPE_F16: ggml_sys::ggml_type = ggml_sys::GGML_TYPE_F16;
+/// Float 32-bit.
 pub const TYPE_F32: ggml_sys::ggml_type = ggml_sys::GGML_TYPE_F32;
 
 /// Acts as a RAII-guard over a `ggml_sys::ggml_context`, allocating via
-/// ggml_init and dropping via ggml_free
+/// `ggml_init` and dropping via `ggml_free`.
 pub struct Context {
     /// An `Arc` is used to model the relation between the context and the
-    /// allocated tensors. Tensors are owned by the object, so a [`GgmlTensor`]
+    /// allocated tensors. Tensors are owned by the object, so a [`Tensor`]
     /// contains a `Weak` reference underneath and doesn't let you do anything
     /// with it if the underlying context has been deallocated.
     ptr: Arc<NonNull<ggml_sys::ggml_context>>,
 }
 impl Context {
+    /// Creates a new [Context] with the specified `mem_size` as a working area.
     pub fn init(mem_size: usize) -> Self {
         let raw = unsafe {
             ggml_sys::ggml_init(ggml_sys::ggml_init_params {
@@ -41,6 +60,7 @@ impl Context {
         }
     }
 
+    /// Wraps a raw tensor with a weak pointer to the context.
     fn new_tensor_raw(&self, raw: *mut ggml_sys::ggml_tensor) -> Tensor {
         Tensor {
             ptr: NonNull::new(raw).expect("Should not be null"),
@@ -48,95 +68,118 @@ impl Context {
         }
     }
 
+    /// Creates a new 1D tensor.
     pub fn new_tensor_1d(&self, typ: ggml_sys::ggml_type, ne0: i32) -> Tensor {
         let raw = unsafe { ggml_sys::ggml_new_tensor_1d(self.ptr.as_ptr(), typ, ne0) };
         self.new_tensor_raw(raw)
     }
 
+    /// Creates a new 2D tensor.
     pub fn new_tensor_2d(&self, typ: ggml_sys::ggml_type, ne0: i32, ne1: i32) -> Tensor {
         let raw = unsafe { ggml_sys::ggml_new_tensor_2d(self.ptr.as_ptr(), typ, ne0, ne1) };
         self.new_tensor_raw(raw)
     }
 
+    /// Creates a new 3D tensor.
     pub fn new_tensor_3d(&self, typ: ggml_sys::ggml_type, ne0: i32, ne1: i32, ne2: i32) -> Tensor {
         let raw = unsafe { ggml_sys::ggml_new_tensor_3d(self.ptr.as_ptr(), typ, ne0, ne1, ne2) };
         self.new_tensor_raw(raw)
     }
 
+    /// Creates a new 1D tensor with the specified value.
     pub fn new_f32(&self, x: f32) -> Tensor {
         let raw = unsafe { ggml_sys::ggml_new_f32(self.ptr.as_ptr(), x) };
         self.new_tensor_raw(raw)
     }
 
+    /// Unknown.
     pub fn op_get_rows(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_get_rows(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the values of `a`, but normalized.
     pub fn op_norm(&self, a: &Tensor) -> Tensor {
         let tensor = unsafe { ggml_sys::ggml_norm(self.ptr.as_ptr(), a.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the multiplication of `a` and `b`.
     pub fn op_mul(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_mul(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Unknown.
     pub fn op_repeat(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_repeat(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the multiplication of `a` and `b` as if they were matrices.
+    ///
+    /// `a`: m rows, n columns
+    ///
+    /// `b`: p rows, n columns (i.e. we transpose it internally)
+    ///
+    /// Result is m columns, p rows
     pub fn op_mul_mat(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_mul_mat(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the addition of `a` and `b`.
     pub fn op_add(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_add(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the [SiLU](https://pytorch.org/docs/stable/generated/torch.nn.SiLU.html) activation function applied to `a`.
     pub fn op_silu(&self, a: &Tensor) -> Tensor {
         let tensor = unsafe { ggml_sys::ggml_silu(self.ptr.as_ptr(), a.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// In-place, scales `a` by the 1D tensor `b`.
     pub fn op_scale(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_scale(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// In-place, sets the elements above the diagonal to -INF.
     pub fn op_diag_mask_inf(&self, a: &Tensor, n_past: i32) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_diag_mask_inf(self.ptr.as_ptr(), a.ptr.as_ptr(), n_past) };
         self.new_tensor_raw(tensor)
     }
 
+    /// In-place, applies the [Softmax function](https://en.wikipedia.org/wiki/Softmax_function) to `a`.
     pub fn op_soft_max(&self, a: &Tensor) -> Tensor {
         let tensor = unsafe { ggml_sys::ggml_soft_max(self.ptr.as_ptr(), a.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a 1D view over `a`.
     pub fn op_view_1d(&self, a: &Tensor, ne0: i32, offset: usize) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_view_1d(self.ptr.as_ptr(), a.ptr.as_ptr(), ne0, offset) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Copies `a` to `b` and returns `b`.
     pub fn op_cpy(&self, a: &Tensor, b: &Tensor) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_cpy(self.ptr.as_ptr(), a.ptr.as_ptr(), b.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Creates a new tensor with the axes of `a` permuted as described by the parameters.
     pub fn op_permute(&self, a: &Tensor, axis0: i32, axis1: i32, axis2: i32, axis3: i32) -> Tensor {
         let tensor = unsafe {
             ggml_sys::ggml_permute(
@@ -150,24 +193,29 @@ impl Context {
         };
         self.new_tensor_raw(tensor)
     }
+
+    /// In-place; reshapes `a` in accordance with the specified dimensions.
     pub fn op_reshape_3d(&self, a: &Tensor, ne0: i32, ne1: i32, ne2: i32) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_reshape_3d(self.ptr.as_ptr(), a.ptr.as_ptr(), ne0, ne1, ne2) };
         self.new_tensor_raw(tensor)
     }
 
+    /// In-place; applies ROtary Positional Encoding.
     pub fn op_rope(&self, a: &Tensor, npast: i32, ndims: i32, mode: i32) -> Tensor {
         let tensor =
             unsafe { ggml_sys::ggml_rope(self.ptr.as_ptr(), a.ptr.as_ptr(), npast, ndims, mode) };
         self.new_tensor_raw(tensor)
     }
 
+    /// Computes the specified graph. Must be run in order to evaluate the graph.
     pub fn graph_compute(&self, graph: &mut ComputationGraph) {
         unsafe {
             ggml_sys::ggml_graph_compute(self.ptr.as_ptr(), &mut graph.inner);
         }
     }
 
+    /// Retrieves the memory used by this [Context].
     pub fn used_mem(&self) -> usize {
         unsafe { ggml_sys::ggml_used_mem(self.ptr.as_ptr()) }
     }
@@ -207,6 +255,7 @@ impl Tensor {
         }
     }
 
+    /// Number of bytes used by this tensor.
     pub fn nbytes(&self) -> usize {
         self.with_alive_ctx(|| {
             // SAFETY: The with_alive_call guarantees the context is alive
@@ -214,6 +263,11 @@ impl Tensor {
         })
     }
 
+    /// Provides raw mutable access to the data contained within the tensor.
+    ///
+    /// # Safety
+    ///
+    /// The data must not be mutated while being read from.
     pub unsafe fn data(&self) -> *mut c_void {
         self.with_alive_ctx(|| {
             // SAFETY: The with_alive_call guarantees the context is alive
@@ -221,6 +275,7 @@ impl Tensor {
         })
     }
 
+    /// Number of elements in this tensor.
     pub fn nelements(&self) -> i32 {
         self.with_alive_ctx(|| {
             // SAFETY: The with_alive_call guarantees the context is alive
@@ -228,42 +283,58 @@ impl Tensor {
         })
     }
 
+    /// Number of elements in each dimension.
     pub fn get_ne(&self) -> [i32; 4] {
         self.with_alive_ctx(|| unsafe { *self.ptr.as_ptr() }.ne)
     }
 
+    /// Stride of each dimension.
     pub fn get_nb(&self) -> [usize; 4] {
         self.with_alive_ctx(|| unsafe { *self.ptr.as_ptr() }.nb)
     }
 
+    /// The data type.
     pub fn get_type(&self) -> ggml_sys::ggml_type {
         self.with_alive_ctx(|| unsafe { *self.ptr.as_ptr() }.type_)
     }
 
+    /// The size of the element type in bytes.
     pub fn element_size(&self) -> usize {
         self.with_alive_ctx(|| unsafe { ggml_sys::ggml_element_size(self.ptr.as_ptr()) })
     }
 
+    /// Writes `src` to this tensor.
+    ///
+    /// # Safety
+    ///
+    /// This tensor must not be written to or read by from any other code.
     pub unsafe fn write_data(&self, src: &[u8]) {
         std::ptr::copy_nonoverlapping(src.as_ptr(), self.data() as *mut u8, src.len())
     }
 
-    #[allow(unused)]
+    /// Zeroes out this tensor.
     pub fn zero_data(&self) {
         unsafe { std::ptr::write_bytes(self.data() as *mut u8, 0, self.nbytes()) }
     }
 
+    /// Reads this tensor into `dst`, starting from `offset`.
+    ///
+    /// # Safety
+    ///
+    /// This tensor must not be written to or read by from any other code.
     pub unsafe fn read_data(&self, offset: usize, dst: &mut [u8]) {
         let data = unsafe { ggml_sys::ggml_get_data(self.ptr.as_ptr()).add(offset) };
         std::ptr::copy_nonoverlapping(data, dst as *mut _ as _, dst.len())
     }
 }
 
+/// A `ggml` computation graph. Keeps track of all state during computation.
 pub struct ComputationGraph {
     inner: ggml_sys::ggml_cgraph,
 }
 
 impl ComputationGraph {
+    /// Create a new [ComputationGraph] with the specified `n_threads`.
     pub fn new(n_threads: i32) -> Self {
         Self {
             inner: ggml_sys::ggml_cgraph {
@@ -275,19 +346,23 @@ impl ComputationGraph {
         }
     }
 
+    /// Build this computational graph in the forward direction in preparation for computation.
     pub fn build_forward_expand(&mut self, tensor: &Tensor) {
         unsafe { ggml_sys::ggml_build_forward_expand(&mut self.inner, tensor.ptr.as_ptr()) }
     }
 }
 
+/// The size of `t` as bytes.
 pub fn type_size(t: Type) -> usize {
     unsafe { ggml_sys::ggml_type_size(t) }
 }
 
+/// [type_size]/[blck_size] as float.
 pub fn type_sizef(x: ggml_sys::ggml_type) -> f64 {
     (unsafe { ggml_sys::ggml_type_sizef(x) }) as f64
 }
 
+/// The size of a block for `t`. Only relevant for quantized types.
 pub fn blck_size(t: Type) -> i32 {
     unsafe { ggml_sys::ggml_blck_size(t) }
 }
