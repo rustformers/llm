89401073463e747eb533c3da583deeece049976b
Jack Michaud <jack@lomz.me>
2023-03-26 08:45:51 -0400

    refactor: move model and session instantiation into InferenceSessionManager

--- llama-http/src/inference.rs
+++ llama-http/src/inference.rs
@@ -1,6 +1,6 @@
 use llama_rs::{
-    InferenceParameters, InferenceSessionParameters, InferenceSnapshot, LoadProgress,
-    ModelKVMemoryType, TokenBias,
+    InferenceParameters, InferenceSession, InferenceSessionParameters, InferenceSnapshot,
+    LoadProgress, ModelKVMemoryType, TokenBias,
 };
 use rand::thread_rng;
 use std::convert::Infallible;
@@ -30,8 +30,80 @@ pub fn initialize_model_and_handle_inferences() -> Sender<InferenceRequest> {
     std::thread::spawn(move || {
         let args = &*CLI_ARGS;
 
+        let mut inference_session_manager = InferenceSessionManager::new();
+
+        let rx: Receiver<InferenceRequest> = rx.clone();
+        loop {
+            if let Ok(inference_request) = rx.try_recv() {
+                let mut session = inference_session_manager.get_session();
+                let inference_params = InferenceParameters {
+                    n_threads: args.num_threads as i32,
+                    n_batch: inference_request.n_batch.unwrap_or(args.batch_size),
+                    top_k: inference_request.top_k.unwrap_or(args.top_k),
+                    top_p: inference_request.top_p.unwrap_or(args.top_p),
+                    repeat_penalty: inference_request
+                        .repeat_penalty
+                        .unwrap_or(args.repeat_penalty),
+                    temp: inference_request.temp.unwrap_or(args.temp),
+                    bias_tokens: TokenBias::default(),
+                };
+                let mut rng = thread_rng();
+                // Run inference
+                let model = &(inference_session_manager.model);
+                let vocabulary = &(inference_session_manager.vocabulary);
+                session
+                    .inference_with_prompt::<Infallible>(
+                        model,
+                        vocabulary,
+                        &inference_params,
+                        &inference_request.prompt,
+                        inference_request.num_predict,
+                        &mut rng,
+                        {
+                            let tx_tokens = inference_request.tx_tokens.clone();
+                            move |t| {
+                                let text = t.to_string();
+                                match tx_tokens.send(Ok(text)) {
+                                    Ok(_) => {
+                                        log::debug!("Sent token {} to receiver.", t);
+                                    }
+                                    Err(_) => {
+                                        // The receiver has been dropped.
+                                        log::warn!("Could not send token to receiver.");
+                                    }
+                                }
+
+                                Ok(())
+                            }
+                        },
+                    )
+                    .expect("Could not run inference");
+            }
+
+            std::thread::sleep(std::time::Duration::from_millis(5));
+        }
+    });
+
+    tx
+}
+
+/// `InferenceSessionManager` is a way to create new sessions for a model and vocabulary.
+/// In the future, it can also manage how many sessions are created and manage creating sessions
+/// between threads.
+struct InferenceSessionManager {
+    model: llama_rs::Model,
+    vocabulary: llama_rs::Vocabulary,
+}
+
+impl InferenceSessionManager {
+    fn new() -> Self {
+        // TODO It's not a great pattern to inject these arguments from CLI_ARGS.
+        // If we ever wanted to support this struct in multiple places, please move the `args`
+        // variable into properties of this struct.
+        let args = &*CLI_ARGS;
+
         // Load model
-        let (mut model, vocabulary) =
+        let (model, vocabulary) =
             llama_rs::Model::load(&args.model_path, args.num_ctx_tokens as i32, |progress| {
                 match progress {
                     LoadProgress::HyperparametersLoaded(hparams) => {
@@ -84,16 +156,24 @@ pub fn initialize_model_and_handle_inferences() -> Sender<InferenceRequest> {
             })
             .expect("Could not load model");
 
-        let mut session = if let Some(restore_path) = &args.restore_prompt {
+        Self { model, vocabulary }
+    }
+
+    fn get_session(&mut self) -> InferenceSession {
+        // TODO It's not a great pattern to inject these arguments from CLI_ARGS.
+        // If we ever wanted to support this struct in multiple places, please move the `args`
+        // variable into properties of this struct.
+        let args = &*CLI_ARGS;
+
+        if let Some(restore_path) = &args.restore_prompt {
             let snapshot = InferenceSnapshot::load_from_disk(restore_path);
-            match snapshot.and_then(|snapshot| model.session_from_snapshot(snapshot)) {
+            match snapshot.and_then(|snapshot| self.model.session_from_snapshot(snapshot)) {
                 Ok(session) => {
                     log::info!("Restored cached memory from {restore_path}");
                     session
                 }
                 Err(err) => {
-                    log::error!("Could not restore from snapshot. Error: {err}");
-                    std::process::exit(1);
+                    panic!("Could not restore from snapshot. Error: {err}");
                 }
             }
         } else {
@@ -109,58 +189,7 @@ pub fn initialize_model_and_handle_inferences() -> Sender<InferenceRequest> {
                     last_n_size: args.repeat_last_n,
                 }
             };
-            model.start_session(inference_session_params)
-        };
-
-        let mut rng = thread_rng();
-        let rx: Receiver<InferenceRequest> = rx.clone();
-        loop {
-            if let Ok(inference_request) = rx.try_recv() {
-                let inference_params = InferenceParameters {
-                    n_threads: args.num_threads as i32,
-                    n_batch: inference_request.n_batch.unwrap_or(args.batch_size),
-                    top_k: inference_request.top_k.unwrap_or(args.top_k),
-                    top_p: inference_request.top_p.unwrap_or(args.top_p),
-                    repeat_penalty: inference_request
-                        .repeat_penalty
-                        .unwrap_or(args.repeat_penalty),
-                    temp: inference_request.temp.unwrap_or(args.temp),
-                    bias_tokens: TokenBias::default(),
-                };
-
-                // Run inference
-                session
-                    .inference_with_prompt::<Infallible>(
-                        &model,
-                        &vocabulary,
-                        &inference_params,
-                        &inference_request.prompt,
-                        inference_request.num_predict,
-                        &mut rng,
-                        {
-                            let tx_tokens = inference_request.tx_tokens.clone();
-                            move |t| {
-                                let text = t.to_string();
-                                match tx_tokens.send(Ok(text)) {
-                                    Ok(_) => {
-                                        log::debug!("Sent token {} to receiver.", t);
-                                    }
-                                    Err(_) => {
-                                        // The receiver has been dropped.
-                                        log::warn!("Could not send token to receiver.");
-                                    }
-                                }
-
-                                Ok(())
-                            }
-                        },
-                    )
-                    .expect("Could not run inference");
-            }
-
-            std::thread::sleep(std::time::Duration::from_millis(5));
+            self.model.start_session(inference_session_params)
         }
-    });
-
-    tx
+    }
 }
