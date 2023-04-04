f30a3c81db91e9d122c25a20ec55c71893aa1e3f
setzer22 <jsanchezfsms@gmail.com>
2023-04-04 20:13:39 +0200

    Address review comments

---
+++ CONTRIBUTING.md
@@ -0,0 +1,20 @@
+# Contributors guide
+
+This document contains a few things that contributors should know about how the
+project is managed. It will be expanded over time with more information.
+
+## Regenerating the GGML Bindings
+
+When new GGML versions are pushed to llama.cpp (or one of the other repos
+hosting a copy of it) and we want to update our copy, the process should be as
+follows:
+
+- Update the `ggml.c` and `ggml.h` inside ggml-sys/ggml.
+- In that same folder, take note of the llama.cpp version these files were taken
+  from inside the `CREDITS.md` file.
+- Run the bindgen script:
+    ```shell
+    $ cargo run --bin generate_ggml_bindings ggml-sys
+    ```
+- Fix any compiler errors that pop up due to the new version of the bindings and
+  test the changes.
--- generate_ggml_bindings/src/main.rs
+++ generate_ggml_bindings/src/main.rs
@@ -1,21 +1,33 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 fn main() {
+    // Parse arguments
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <path_to_ggml_crate>", args[0]);
+        return;
+    }
+
+    let ggml_crate_path = &args[1];
+
+    let header_path = format!("{ggml_crate_path}/ggml/ggml.h");
+
     let bindings = bindgen::Builder::default()
-        .header("ggml/ggml.h")
+        .header(&header_path)
         // Suppress some warnings
         .raw_line("#![allow(non_upper_case_globals)]")
         .raw_line("#![allow(non_camel_case_types)]")
         .raw_line("#![allow(non_snake_case)]")
         .raw_line("#![allow(unused)]")
         // Do not generate code for ggml's includes (stdlib)
-        .allowlist_file("ggml/ggml.h")
+        .allowlist_file(&header_path)
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = env::current_dir().unwrap().join("src");
+    let out_path = PathBuf::from(ggml_crate_path).join("src").join("lib.rs");
     bindings
-        .write_to_file(out_path.join("lib.rs"))
+        .write_to_file(out_path)
         .expect("Couldn't write bindings");
 
     println!("Successfully updated bindings in src/lib.rs");
