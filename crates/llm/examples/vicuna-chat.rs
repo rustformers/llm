da3e3906620b88a310276acd0dc5e81de6083169
LLukas22 <65088241+LLukas22@users.noreply.github.com>
2023-05-12 10:00:07 +0200

    Update vicuna-chat.rs

--- crates/llm/examples/vicuna-chat.rs
+++ crates/llm/examples/vicuna-chat.rs
@@ -132,6 +132,11 @@ fn load_progress_callback(
                 prev_load_time = std::time::Instant::now();
             }
         }
+        LoadProgress::LoraApplied { name } => {
+            if let Some(sp) = sp.as_mut() {
+                sp.update_text(format!("Applied Lora: {}", name));
+            };
+        }
         LoadProgress::Loaded {
             file_size,
             tensor_count,
