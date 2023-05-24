e15781514711397cd878bfd3a4ff592de95f46d3
Philpax <me@philpax.me>
2023-05-25 00:01:36 +0200

    feat(llm): rewrite embeddings example with args

--- crates/llm/examples/embeddings.rs
+++ crates/llm/examples/embeddings.rs
@@ -1,20 +1,35 @@
-use llm::{LoadProgress, Model, ModelArchitecture, ModelParameters, OutputRequest};
-use spinoff::{spinners::Dots2, Spinner};
-use std::{path::Path, time::Instant};
+use std::path::Path;
 
 fn main() {
-    let model_path = Path::new(r"./models/ggml-vicuna-7B-1.1-q5_0.bin");
-    let model_architecture: ModelArchitecture = ModelArchitecture::Llama;
-    let overrides = serde_json::from_str("{}").unwrap();
-    let sp = Some(Spinner::new(Dots2, "Loading model...", None));
+    // Get arguments from command line
+    let raw_args: Vec<String> = std::env::args().skip(1).collect();
+    if raw_args.len() < 2 {
+        println!("Usage: cargo run --release --example embeddings <model_architecture> <model_path> [query] [comma-separated comparands] [overrides, json]");
+        std::process::exit(1);
+    }
 
-    let now = Instant::now();
-    let prev_load_time = now;
+    let model_architecture: llm::ModelArchitecture = raw_args[0].parse().unwrap();
+    let model_path = Path::new(&raw_args[1]);
+    let query = raw_args
+        .get(2)
+        .map(|s| s.as_str())
+        .unwrap_or("My favourite animal is the dog");
+    let comparands = raw_args
+        .get(3)
+        .map(|s| s.split(',').map(|s| s.trim()).collect::<Vec<_>>())
+        .unwrap_or_else(|| {
+            vec![
+                "My favourite animal is the dog",
+                "I have just adopted a cute dog",
+                "My favourite animal is the cat",
+            ]
+        });
+    let overrides = raw_args.get(4).map(|s| serde_json::from_str(s).unwrap());
 
-    let model_params = ModelParameters {
+    // Load model
+    let model_params = llm::ModelParameters {
         prefer_mmap: true,
         context_size: 4096,
-        inference_parameters: Default::default(),
         lora_adapters: None,
     };
     let model = llm::load_dynamic(
@@ -22,78 +37,83 @@ fn main() {
         model_path,
         model_params,
         overrides,
-        load_progress_callback(sp, now, prev_load_time),
+        llm::load_progress_callback_stdout,
     )
     .unwrap_or_else(|err| {
         panic!("Failed to load {model_architecture} model from {model_path:?}: {err}")
     });
+    let inference_parameters = llm::InferenceParameters::default();
 
-    let query = "My favourite animal is the dog";
-    //let query = "What is your favourite animal";
-
-    let sentences = vec![
-        //"My favourite animal is the dog",
-        "I have just adopted a cute dog",
-        "My favourite animal is the cat",
-    ];
-    fn get_embeddings(model: &Box<dyn Model>, query: &str) -> Option<Vec<f32>> {
-        let mut session = model.start_session(Default::default());
-        let mut output_request = OutputRequest {
-            all_logits: None,
-            embeddings: Some(Vec::new()),
-        };
-        let vocab = model.vocabulary();
-        let beginning_of_sentence = true;
-        let query_token_ids = vocab
-            .tokenize(&format!(" {}", query), beginning_of_sentence)
-            .unwrap()
-            .iter()
-            .map(|(_, tok)| *tok)
-            .collect::<Vec<_>>();
-        let _ = model.evaluate(
-            &mut session,
-            //&inference_params,
-            &Default::default(),
-            &query_token_ids,
-            &mut output_request,
-        );
-        output_request.embeddings
-    }
-    let query_embeddings = get_embeddings(&model, query).unwrap();
-    println!(
-        "Text: {:?} Embeddings length: {:?}\nThe first 10 elements of embeddings: \n{:?}\n",
-        query,
-        query_embeddings.len(),
-        query_embeddings.get(0..10)
-    );
-    let mut sentences_similarity = sentences
+    // Generate embeddings for query and comparands
+    let query_embeddings = get_embeddings(model.as_ref(), &inference_parameters, query);
+    let comparand_embeddings: Vec<(String, Vec<f32>)> = comparands
         .iter()
-        .enumerate()
-        .map(|(_idx, &s)| {
-            let embeddings = get_embeddings(&model, s).unwrap();
-            println!(
-                "Text: {:?} Embeddings length: {:?}\nThe first 10 elements of embeddings: \n{:?}\n",
-                s,
-                embeddings.len(),
-                embeddings.get(0..10)
-            );
-            let value = cosine_similarity(&query_embeddings, &embeddings);
-            TextSimilrity {
-                text: s.to_owned(),
-                similarity: value,
-            }
+        .map(|&text| {
+            (
+                text.to_owned(),
+                get_embeddings(model.as_ref(), &inference_parameters, text),
+            )
         })
-        .collect::<Vec<_>>();
-    sentences_similarity.sort_by(|a, b| b.similarity.partial_cmp(&a.similarity).unwrap());
-    sentences_similarity
+        .collect();
+
+    // Print embeddings
+    fn print_embeddings(text: &str, embeddings: &[f32]) {
+        println!("{text}");
+        println!("  Embeddings length: {}", embeddings.len());
+        println!("  Embeddings first 10: {:.02?}", embeddings.get(0..10));
+    }
+
+    print_embeddings(query, &query_embeddings);
+    println!("---");
+    for (text, embeddings) in &comparand_embeddings {
+        print_embeddings(text, embeddings);
+    }
+
+    // Calculate the cosine similarity between the query and each comparand, and sort by similarity
+    let mut similarities: Vec<(&str, f32)> = comparand_embeddings
         .iter()
-        .for_each(|i| println!("Cosine Similrity: {:#?}\n", i));
+        .map(|(text, embeddings)| {
+            (
+                text.as_str(),
+                cosine_similarity(&query_embeddings, &embeddings),
+            )
+        })
+        .collect();
+    similarities.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    // Print similarities
+    println!("---");
+    println!("Similarities:");
+    for (text, score) in similarities {
+        println!("  {text}: {score}");
+    }
 }
 
-#[derive(Debug)]
-pub struct TextSimilrity {
-    pub text: String,
-    pub similarity: f32,
+fn get_embeddings(
+    model: &dyn llm::Model,
+    inference_parameters: &llm::InferenceParameters,
+    query: &str,
+) -> Vec<f32> {
+    let mut session = model.start_session(Default::default());
+    let mut output_request = llm::OutputRequest {
+        all_logits: None,
+        embeddings: Some(Vec::new()),
+    };
+    let vocab = model.vocabulary();
+    let beginning_of_sentence = true;
+    let query_token_ids = vocab
+        .tokenize(&format!(" {}", query), beginning_of_sentence)
+        .unwrap()
+        .iter()
+        .map(|(_, tok)| *tok)
+        .collect::<Vec<_>>();
+    let _ = model.evaluate(
+        &mut session,
+        inference_parameters,
+        &query_token_ids,
+        &mut output_request,
+    );
+    output_request.embeddings.unwrap()
 }
 
 fn cosine_similarity(v1: &[f32], v2: &[f32]) -> f32 {
@@ -110,62 +130,4 @@ fn dot(v1: &[f32], v2: &[f32]) -> f32 {
 
 fn magnitude(v: &[f32]) -> f32 {
     v.iter().map(|&x| x * x).sum::<f32>().sqrt()
-}
-
-fn load_progress_callback(
-    mut sp: Option<Spinner>,
-    now: Instant,
-    mut prev_load_time: Instant,
-) -> impl FnMut(LoadProgress) {
-    move |progress| match progress {
-        LoadProgress::HyperparametersLoaded => {
-            if let Some(sp) = sp.as_mut() {
-                sp.update_text("Loaded hyperparameters")
-            };
-        }
-        LoadProgress::ContextSize { bytes } => log::debug!(
-            "ggml ctx size = {}",
-            bytesize::to_string(bytes as u64, false)
-        ),
-        LoadProgress::TensorLoaded {
-            current_tensor,
-            tensor_count,
-            ..
-        } => {
-            if prev_load_time.elapsed().as_millis() > 500 {
-                // We don't want to re-render this on every message, as that causes the
-                // spinner to constantly reset and not look like it's spinning (and
-                // it's obviously wasteful).
-                if let Some(sp) = sp.as_mut() {
-                    sp.update_text(format!(
-                        "Loaded tensor {}/{}",
-                        current_tensor + 1,
-                        tensor_count
-                    ));
-                };
-                prev_load_time = std::time::Instant::now();
-            }
-        }
-        LoadProgress::LoraApplied { name, source } => {
-            if let Some(sp) = sp.as_mut() {
-                sp.update_text(format!(
-                    "Applied LoRA: {} Source: {}",
-                    name,
-                    source.to_string_lossy()
-                ));
-            };
-        }
-        LoadProgress::Loaded {
-            file_size,
-            tensor_count,
-        } => {
-            if let Some(sp) = sp.take() {
-                sp.success(&format!(
-                    "Loaded {tensor_count} tensors ({}) after {}ms",
-                    bytesize::to_string(file_size, false),
-                    now.elapsed().as_millis()
-                ));
-            };
-        }
-    }
 }
