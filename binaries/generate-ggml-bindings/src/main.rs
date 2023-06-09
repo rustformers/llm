52bc0046bc38ccaf4464b2ef2807a904921864c0
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2023-06-09 16:54:32 +0200

    hack to enable generation of bindings on windows

--- binaries/generate-ggml-bindings/src/main.rs
+++ binaries/generate-ggml-bindings/src/main.rs
@@ -2,6 +2,7 @@
 //!
 //! Assumed to be run from the root of the workspace.
 
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
@@ -22,9 +23,28 @@ fn main() {
         .join("sys")
         .join("src")
         .join("lib.rs");
-    bindings
-        .write_to_file(out_path)
-        .expect("Couldn't write bindings");
+
+    let mut generated_bindings = bindings.to_string();
+
+    if cfg!(windows) {
+        // windows generates all ::std::os::raw::c_* enum types as i32.
+        // We need to replace some of them with c_uint as the rust bindings expect them to be unsigned.
+        // Temporary hack until bindgen supports defining the enum types manually. See https://github.com/rust-lang/rust-bindgen/issues/1907
+        generated_bindings = generated_bindings.replace(
+            "ggml_type = ::std::os::raw::c_int;",
+            "ggml_type = ::std::os::raw::c_uint;",
+        );
+        generated_bindings = generated_bindings.replace(
+            "ggml_backend = ::std::os::raw::c_int;",
+            "ggml_backend = ::std::os::raw::c_uint;",
+        );
+        generated_bindings = generated_bindings.replace(
+            "ggml_op = ::std::os::raw::c_int;",
+            "ggml_op = ::std::os::raw::c_uint;",
+        );
+    }
+
+    fs::write(out_path, generated_bindings).expect("Couldn't write bindings");
 
     println!("Successfully updated bindings");
 }
