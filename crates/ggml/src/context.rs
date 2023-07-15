dadb1044438cc919e07572809ac5a710653462a8
skirodev <sinpx@foxmail.com>
2023-07-15 18:34:38 +0800

    add Falcon 40B model support

--- crates/ggml/src/context.rs
+++ crates/ggml/src/context.rs
@@ -473,6 +473,12 @@ impl Context {
         let tensor = unsafe { sys::ggml_gelu(self.ptr.as_ptr(), a.ptr.as_ptr()) };
         self.new_tensor_raw(tensor)
     }
+
+    /// flash attention.
+    pub fn op_flash_attn(&self, q: &Tensor, k: &Tensor, v: &Tensor, masked: bool) -> Tensor {
+        let tensor = unsafe { sys::ggml_flash_attn(self.ptr.as_ptr(), q.ptr.as_ptr(), k.ptr.as_ptr(), v.ptr.as_ptr(), masked) };
+        self.new_tensor_raw(tensor)
+    }
 }
 
 impl Drop for Context {
